a5d9100cc99ad96706c3e02dbbb280c3da949c17
Vedant2210 <120133071+Vedant2210@users.noreply.github.com>
2025-02-15 12:40:43 +0530

    Add files via upload

---
+++ substrig.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str="Vedant Nigam";
+    cout<<str.substr(1,60)<<endl; //edant Nigam
+    cout<<str.substr(1)<<endl;//edant Nigam
+    cout<<str.substr(1,7)<<endl; //edant N
+}
