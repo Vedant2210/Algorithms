ce3f529eff4e81fbaed4bdf82c83e00b251496d4
Vedant2210 <120133071+Vedant2210@users.noreply.github.com>
2025-02-20 19:52:31 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //prefix
+    // int a=5;
+    // cout<<a<<endl;
+    // cout<<++a<<endl;
+    // cout<<a<<endl;
+    // cout<<--a<<endl;
+    // cout<<a<<endl;
+
+
+    int a=5;
+    cout<<a<<endl;
+    cout<<a++<<endl;
+    cout<<a<<endl;
+    cout<<a--<<endl;
+    cout<<a<<endl;
+
+}
