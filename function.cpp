1c112c1849f6718230256ca655d343c28247f396
Vedant2210 <120133071+Vedant2210@users.noreply.github.com>
2025-03-04 11:23:55 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int,int);
+int main(){
+    // int add(int,int);
+    void func(void);
+    func();
+    int x=add(10,20);
+    cout<<x<<endl;
+}
+
+void func(void){
+    cout<<"hi"<<endl;
+}
+
+int add(int x,int y){
+    return x+y;
+}
