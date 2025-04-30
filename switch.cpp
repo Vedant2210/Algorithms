204a9a08a4963fbdfd79739c5a7346c53e59eb42
Vedant2210 <120133071+Vedant2210@users.noreply.github.com>
2025-04-30 19:56:02 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    switch(x){
+        
+        
+        case 2:
+        cout<<"second "<<x<<endl;
+       
+        
+        default:
+        cout<<"default"<<endl;
+        case 1:
+        cout<<"first "<<x<<endl;
+    }
+}
